9e61bfd576b50d349bcdde9b66c8dac42de8187e
Matheus Rubens <matheus.rubens@opencashback.com.br>
2024-10-21 21:05:14 -0300

    .

--- front/app/routes.js
+++ front/app/routes.js
@@ -1,26 +1,13 @@
-angular.module('meuApp', ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider) {
-    // Redireciona para a dashboard por padrão
-    $urlRouterProvider.otherwise('/dashboard');
-
-    $stateProvider
-        .state('main', {
-            abstract: true,
-            templateUrl: 'app/partials/main-layout.html', // Template com header, sidebar e footer
-        })
-        .state('main.dashboard', {
-            url: '/dashboard',
-            templateUrl: 'app/partials/dashboard.html', // Template da dashboard
-            controller: 'DashboardController' // Controller para a dashboard
-        })
-        .state('login', {
-            url: '/login',
-            templateUrl: 'app/partials/login.html', // Apenas um conteúdo sem partials
-            controller: 'LoginController' // Controller para o login
-        })
-        .state('main.produtos', {
-            url: '/produtos',
-            templateUrl: 'app/partials/produtos.html', // Outra página que usa o layout com partials
-            controller: 'ProdutosController'
-        });
-});
+app.config(function($routeProvider){
+    // $routeProvider.when('/', {templateUrl: 'app/views/home.html', controller:'testeController'})
+    $routeProvider
+    .when('/',
+        {
+            templateUrl: 'app/views/home.html',
+            controller:'testeController'
+        }
+    )
+    .otherwise({
+        redirectTo: '/'
+    });
+})
