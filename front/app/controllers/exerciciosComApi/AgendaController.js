fe3aa4767f49564bd012ca125e7c4c00a433004e
Matheus Rubens <matheus.rubens@opencashback.com.br>
2024-11-11 20:31:06 -0300

    instrucoes

--- front/app/controllers/exerciciosComApi/AgendaController.js
+++ front/app/controllers/exerciciosComApi/AgendaController.js
@@ -1,4 +1,16 @@
 angular.module('meuApp')
     .controller('AgendaController', function ($scope, $http) {
 
+/*
+1- fazer a consulta carregar logo de cara (cria uma funcao e chama ela direto)
+2- a consulta funcionando, criaremos 2 botoes depois Editar e excluir (sem funcionar ainda)
+3- Apos fazer o 1 e 2, o botão Adicionar / editar deve criar um novo dado
+4- faremos o excluir funcionar
+5- o editar deve carregar as informaçoes nos campos de texto, mudar o texto "Novo" para "Editando id #id"
+6- o botao adicionar deve mudar para editar
+7- quando estiver escrito editar, deve editar, se estiver escrito adicionar, adicionar
+8- teremos uma agenda funcional <3
+*/
+
+
     });
