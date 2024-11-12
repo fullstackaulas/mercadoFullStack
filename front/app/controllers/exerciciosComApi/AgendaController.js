b679e5f89d0cbdfe2ecbbd7ce0945af2e8b85244
Matheus Rubens <matheus.rubens@opencashback.com.br>
2024-11-11 21:10:31 -0300

    funcionando ou nao

--- front/app/controllers/exerciciosComApi/AgendaController.js
+++ front/app/controllers/exerciciosComApi/AgendaController.js
@@ -1,16 +1,35 @@
 angular.module('meuApp')
     .controller('AgendaController', function ($scope, $http) {
+        // <th>Nome</th>
+        // <th>Telefone Fixo</th>
+        // <th>Telefone Celular</th>
+        // <th>Email</th>
+        // <th>Endereço</th>
+        // <th>Ações</th>
+
+        // $scope.contatos = [{
+        //     nome: 'Matheus',
+        //     email: 'matheus.rubens@bol.com.br',
+        //     telefoneFixo: '1124242424',
+        //     telefoneCelular: '11980802497',
+        //     endereco: 'Rua palindromo'
+        // },
+        // {
+        //     nome: 'Matheus 2',
+        //     email: 'matheus2.rubens@bol.com.br',
+        //     telefoneFixo: '1111111',
+        //     telefoneCelular: '2132132131',
+        //     endereco: 'Rua blablabla'
+        // }];
+
+        $scope.contatos = [];
+
+        console.log($scope.contatos);
+
 
 
         listar = function () {
-            $url = 'http://localhost:8000/api/agenda/listar';
-            $http.get($url).then(function (response) {
-                console.log(response);
-
-            }, function (error) {
-
-                console.log(error);
-            });
+          
 
         }
 
