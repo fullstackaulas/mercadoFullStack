5dca09381ba325b6f42a832f5785a788bbe0b06c
Matheus Rubens <matheus.rubens@opencashback.com.br>
2024-11-18 20:33:54 -0300

    .

--- front/app/controllers/exerciciosComApi/AgendaController.js
+++ front/app/controllers/exerciciosComApi/AgendaController.js
@@ -1,5 +1,5 @@
 angular.module('meuApp')
-    .controller('AgendaController', function ($scope, $http) {
+  .controller('AgendaController', function ($scope, $http) {
 
 
     //   Swal.fire({
@@ -8,65 +8,73 @@ angular.module('meuApp')
     //     icon: 'success',
     //     confirmButtonText: 'Ok'
     // });
-    
-        $scope.contatos = [];
 
-        console.log($scope.contatos);
+    $scope.contatos = [];
 
-        listar = function () {
-          $url = 'http://localhost:8000/api/agenda/listar';
-          $http.get($url).then(function(response){
-            console.log(response);
-            $scope.contatos = response.data;
+    console.log($scope.contatos);
 
-          },function(error){
-            console.log(error);
-          });
+    listar = function () {
+      $url = 'http://localhost:8000/api/agenda/listar';
+      $http.get($url).then(function (response) {
+        console.log(response);
+        $scope.contatos = response.data;
 
-        }
+      }, function (error) {
+        console.log(error);
+      });
 
-        listar();
+    }
 
-        $scope.dados = {
-        id:'',
-        nome: '',
-        telefoneFixo:'',
-        telefoneCelular:'',
-        email:'',
-        endereco:''
+    listar();
+
+    $scope.dados = {
+      id: '',
+      nome: '',
+      telefoneFixo: '',
+      telefoneCelular: '',
+      email: '',
+      endereco: ''
     };
 
-    $scope.salvarInfo = function(){
-        $url = 'http://localhost:8000/api/agenda/salvar';
-        $http.post($url,$scope.dados).then(function(response){
-            console.log(response);
-
-        }, function(error){
-            console.log(error);
+    $scope.salvarInfo = function () {
+      $url = 'http://localhost:8000/api/agenda/salvar';
+      $http.post($url, $scope.dados).then(function (response) {
+        console.log(response);
+        if (response.status == 201) {
+          Swal.fire({
+          title: 'Sucesso!',
+            text: 'Operação realizada com sucesso!',
+              icon: 'success',
+                confirmButtonText: 'Ok'
         });
+    }
+
+  }, function (error) {
+    console.log(error);
+  });
     };
 
-    $scope.delete = function(id) {
-      $url = 'http://localhost:8000/api/agenda/deletar/'+ id;
-      $http.delete($url).then(function(response){
-        console.log(response);
-      
-      },function(error){
-          console.log(error);
-      });
-    };
-    
-    $scope.editar = function(id) {
-      $url = 'http://localhost:8000/api/agenda/lerUm/'+ id;
-      $http.get($url).then(function(response){
-        console.log(response);
-        $scope.dados = response.data;
-      
-      },function(error){
-          console.log(error);
-      });
-    };
-    
+$scope.delete = function (id) {
+  $url = 'http://localhost:8000/api/agenda/deletar/' + id;
+  $http.delete($url).then(function (response) {
+    console.log(response);
+
+  }, function (error) {
+    console.log(error);
+  });
+};
+
+$scope.editar = function (id) {
+  $url = 'http://localhost:8000/api/agenda/lerUm/' + id;
+  $http.get($url).then(function (response) {
+    console.log(response);
+    $scope.dados = response.data;
+
+  }, function (error) {
+    console.log(error);
+  });
+};
+
 
 
         /*
