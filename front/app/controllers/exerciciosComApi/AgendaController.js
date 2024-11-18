ab6932efc370a5436b090955c952ae029030f02f
Matheus Rubens <matheus.rubens@opencashback.com.br>
2024-11-18 18:55:04 -0300

    .

--- front/app/controllers/exerciciosComApi/AgendaController.js
+++ front/app/controllers/exerciciosComApi/AgendaController.js
@@ -1,7 +1,6 @@
 angular.module('meuApp')
     .controller('AgendaController', function ($scope, $http) {
 
-        
         // <th>Nome</th>
         // <th>Telefone Fixo</th>
         // <th>Telefone Celular</th>
