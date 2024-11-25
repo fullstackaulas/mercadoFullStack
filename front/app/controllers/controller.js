767ad2f63287faba607c7cd7b9ee4724d7169214
LeonardoOSilva022 <Leonardo.oliver23@icloud.com>
2024-11-25 20:10:22 -0300

    verificar o usuário logado

--- front/app/controllers/controller.js
+++ front/app/controllers/controller.js
@@ -1,12 +1,15 @@
 angular.module('meuApp')
-.controller('testeController', function($scope) {
+.controller('testeController', function($scope, $state) {
     // Lógica do controlador
 
 $token = localStorage.getItem('token');
 $scope.usuario = JSON.parse(localStorage.getItem('usuario'));
 
-
 console.log($token);
+
+if ($token == null){
+    $state.go('login')
+}
 
 });
 
